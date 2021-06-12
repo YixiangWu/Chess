fdfe9355517d344ee63c2bb3f114e666e0837122
Yixiang <gavinwu964@gmail.com>
2021-06-12 19:50:20 +0800

    Add pawn promotion and right-click highlight
    Allow pawns to promote
    Add a window to prompt players for pawn promotion choices
    Allow players to highlight squares with right clicks

--- game.py
+++ game.py
@@ -30,14 +30,20 @@ class Game(Prep):
     #          56,   57,   58,   59,   60,   61,   62,   63
     #          a1,   b1,   c1,   d1,   e1,   f1,   g1,   h1
 
-    elements = {'turn': 'white', 'en_passant': 99,
-                'castle': {'white': {'long': True, 'short': True},
-                           'black': {'long': True, 'short': True}}}
+    move = []
+    highlights = []
+    turn = 'white'
+    promote = None
+    en_passant = None
+    castle_flags = {'white': {'long': True, 'short': True},
+                    'black': {'long': True, 'short': True}}
 
     def __init__(self):
         super().__init__()
         self.windowSize = (800, 800)
         self.squareSize = (self.windowSize[0] // 8, self.windowSize[1] // 8)
+        self.window = pygame.display.set_mode(self.windowSize)
+        self.images = self.load()
         self.main()
 
     def get_area(self, square):
@@ -46,26 +52,41 @@ class Game(Prep):
                 self.squareSize[1] * (square // 8),
                 self.squareSize[0], self.squareSize[1])
 
+    def get_square(self):
+        """This method turns the click position to a square location."""
+        return (pygame.mouse.get_pos()[0] // self.squareSize[0] +
+                pygame.mouse.get_pos()[1] // self.squareSize[1] * 8)
+
+    @classmethod
+    def update_move(cls, square):
+        """This method updates chess moves."""
+        cls.move.append(square) if square else cls.move.clear()
+
+    @classmethod
+    def update_highlights(cls, square):
+        """This method updates right-click highlights."""
+        cls.highlights.append(square) if square else cls.highlights.clear()
+
     @classmethod
     def update_en_passant(cls, start, target):
         """This method helps update the chess board and game elements."""
-        if cls.elements['en_passant'] != 99:
+        if cls.en_passant:
             # capture the enemy pawn
             if cls.board[start] == 'wP':
-                if target + 8 == cls.elements['en_passant']:
+                if target + 8 == cls.en_passant:
                     cls.board[target + 8] = '00'
             elif cls.board[start] == 'bP':
-                if target - 8 == cls.elements['en_passant']:
+                if target - 8 == cls.en_passant:
                     cls.board[target - 8] = '00'
 
         # en passant capture must be made on the very next turn
-        cls.elements['en_passant'] = 99
+        cls.en_passant = None
 
         # check if a pawn moves forward two squares
         if cls.board[start] == 'wP' and start - target == 16:
-            cls.elements['en_passant'] = target
+            cls.en_passant = target
         elif cls.board[start] == 'bP' and target - start == 16:
-            cls.elements['en_passant'] = target
+            cls.en_passant = target
 
     @classmethod
     def update_castle(cls, start, target):
@@ -81,32 +102,40 @@ class Game(Prep):
                 cls.board[start + 1] = rook
 
             # can't castle if the king has previously moved
-            cls.elements['castle'][cls.elements['turn']]['long'] = False
-            cls.elements['castle'][cls.elements['turn']]['short'] = False
+            cls.castle_flags[cls.turn]['long'] = False
+            cls.castle_flags[cls.turn]['short'] = False
 
         # check if the rook involved has moved or got captured
         if start == 0 or target == 0:
-            cls.elements['castle']['black']['long'] = False
+            cls.castle_flags['black']['long'] = False
         if start == 7 or target == 7:
-            cls.elements['castle']['black']['short'] = False
+            cls.castle_flags['black']['short'] = False
         if start == 56 or target == 56:
-            cls.elements['castle']['white']['long'] = False
+            cls.castle_flags['white']['long'] = False
         if start == 63 or target == 63:
-            cls.elements['castle']['white']['short'] = False
+            cls.castle_flags['white']['short'] = False
 
     @classmethod
-    def update(cls, start=64, target=64):
+    def update_game(cls, start, target, symbol=''):
         """This method updates the chess board and game elements."""
         if start != target:
             cls.update_en_passant(start, target)
             cls.update_castle(start, target)
             cls.board[target] = cls.board[start]
             cls.board[start] = '00'
-            if cls.elements['turn'] == 'white':
-                cls.elements['turn'] = 'black'
-            else:
-                cls.elements['turn'] = 'white'
-        return cls.board, cls.elements
+            cls.turn = 'black' if cls.turn == 'white' else 'white'
+        if symbol:  # update pawn promotions
+            cls.board[target] = symbol
+        for i in range(8):
+            # check if one of the white pawns gets to eighth rank
+            if cls.board[i] == 'wP':
+                cls.promote = i
+                break
+            # check if one of the black pawns gets to first rank
+            elif cls.board[56 + i] == 'bP':
+                cls.promote = 56 + i
+                break
+            cls.promote = None
 
     def load(self):
         """This method loads piece images."""
@@ -121,67 +150,96 @@ class Game(Prep):
                 images[piece] = pygame.transform.scale(img, self.squareSize)
         return images
 
-    def draw(self, window, board, images):
+    def draw(self):
         """This method draws the chess board and pieces."""
         colors = [(220, 230, 230), (120, 150, 170)]
         # light square RGB: 220, 230, 230; dark square RGB: 120, 150, 170
         for square in range(64):
-            piece = board[square]
+            piece = Game.board[square]
             if (square // 8) % 2 == 0:  # first squares on odd ranks are light
                 color = colors[square % 2]
             else:  # first squares on even ranks are dark
                 color = colors[(square + 1) % 2]
-            pygame.draw.rect(window, color, self.get_area(square))
+            pygame.draw.rect(self.window, color, self.get_area(square))
             if piece != '00':
-                window.blit(images[piece], self.get_area(square))
+                self.window.blit(self.images[piece], self.get_area(square))
+        if Game.promote:  # draw a window for pawn promotion prompt
+            promote_window = pygame.Surface(
+                (self.squareSize[0], self.windowSize[1] // 2))
+            promote_window.fill((225, 225, 225))
+            color = 'white' if Game.board[Game.promote] == 'wP' else 'black'
+            square = Game.promote if color == 'white' else Game.promote - 24
+            choices = self.pieceColor[color][1:5]  # promotion choices
+            if color == 'white':
+                choices = reversed(choices)
+            self.window.blit(promote_window, self.get_area(square)[:2])
+            for index, symbol in enumerate(choices):
+                self.window.blit(self.images[symbol],
+                                 self.get_area(square + 8 * index))
 
-    def highlight(self, window, board, images, square, castle, en_passant):
-        """This method highlights the selected square and its legal squares."""
-        pygame.draw.rect(window, (200, 200, 255), self.get_area(square))
-        # highlight the selected square with RGB 200, 200, 255
-        window.blit(images[board[square]], self.get_area(square))
-        for square in self.legal(board, square, castle, en_passant):
-            pygame.draw.rect(window, (255, 100, 130), self.get_area(square))
-            # highlight legal squares with RGB 255, 100, 130
-            if board[square] != '00':
-                window.blit(images[board[square]], self.get_area(square))
+    def highlight(self, square, color):
+        """This method highlights the specify square."""
+        symbol = Game.board[square]
+        pygame.draw.rect(self.window, color, self.get_area(square))
+        if symbol != '00':
+            self.window.blit(self.images[symbol], self.get_area(square))
 
-    def click(self, window, board, images, squares, elements):
+    def make_move(self):
+        """This method makes chess moves."""
+        square = self.get_square()
+        castle, passant = Game.castle_flags[Game.turn], Game.en_passant
+        if not Game.move:  # handles the start square of a chess move
+            if Game.board[square] in self.pieceColor[Game.turn]:
+                Game.update_move(square)
+                start = Game.move[0]
+                self.highlight(start, (200, 200, 255))
+                for square in self.legal(Game.board, start, castle, passant):
+                    self.highlight(square, (100, 190, 150))
+                return
+        else:  # handles the target square of a chess move
+            Game.update_move(square)
+            start, target = Game.move
+            if target in self.legal(Game.board, start, castle, passant):
+                Game.update_game(start, target)
+        self.draw()
+        Game.update_move(None)
+
+    def pawn_promotion(self):
+        """This method handles pawn promotions."""
+        square, symbol = self.get_square(), ''
+        if Game.board[Game.promote] == 'wP':  # handles white pawn promotions
+            if square % 8 == Game.promote and square // 8 <= 3:
+                symbol = self.pieceColor['white'][4 - square // 8]
+        else:  # handles black pawn promotions
+            if square % 8 == Game.promote - 56 and square // 8 >= 4:
+                symbol = self.pieceColor['black'][square // 8 - 3]
+        Game.update_game(Game.promote, Game.promote, symbol)
+        self.draw()
+
+    def click(self, event):
         """This method handles mouse clicks."""
-        square = pygame.mouse.get_pos()[0] // self.squareSize[0] + \
-            pygame.mouse.get_pos()[1] // self.squareSize[1] * 8
-        castle = elements['castle'][elements['turn']]
-        passant = elements['en_passant']
-        squares.append(square)
-        if len(squares) == 1:
-            start = squares[0]
-            if board[start] in self.pieceColor[elements['turn']]:
-                self.highlight(window, board, images, start, castle, passant)
-                return squares, elements
-        else:
-            start, target = squares
-            if target in self.legal(board, start, castle, passant):
-                board, elements = Game.update(start, target)
-        self.draw(window, board, images)
-        return [], elements
+        if event.button == 1:  # left click
+            self.update_highlights(None)
+            self.make_move() if Game.promote is None else self.pawn_promotion()
+        elif event.button == 3:  # right click
+            self.draw()
+            self.update_highlights(self.get_square())
+            for square in Game.highlights:
+                # highlight right clicks with RGB 255, 100, 130
+                self.highlight(square, (255, 100, 130))
 
     def main(self):
         """This method takes user inputs, draws and updates the chess board."""
         pygame.init()
         pygame.display.set_caption('chess')
-        window = pygame.display.set_mode(self.windowSize)
-        board, elements = Game.update()
-        images = self.load()
-        squares = []
-        self.draw(window, board, images)
+        self.draw()
         while True:
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    squares, elements = self.click(
-                        window, board, images, squares, elements)
+                    self.click(event)
 
 
 if __name__ == '__main__':
