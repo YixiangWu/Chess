4ecddee781c85519240f1ce9662f2d3349d34426
Yixiang <gavinwu964@gmail.com>
2021-07-27 18:53:55 -0400

    Add menu page
    A better solution to build menu pages

--- menu.py
+++ menu.py
@@ -19,13 +19,13 @@ class Menu(Game):
         self.menu()
 
     def draw_title_surface(self):
-        """Draw a title surface."""
+        """Draw title surface."""
         title_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT // 5 * 2))
         title_surface.fill((255, 220, 140))
         self.window.blit(title_surface, (0, 0))
 
     def draw_texture(self):
-        """Draw textures."""
+        """Draw texture."""
         for ratio in range(1, 4):
             interval, color = 13, (0, 130, 130)
             pos_x = WINDOW_WIDTH * ratio // interval
@@ -37,79 +37,58 @@ class Menu(Game):
                 (WINDOW_WIDTH - pos_x, WINDOW_HEIGHT), 3)
 
     def draw_text(self, size, text, position):
-        """Draw texts."""
+        """Draw text."""
         font = self.__dict__['font' + size]
         self.window.blit(font.render(text, True, (0, 0, 0)),
                          ((WINDOW_WIDTH - font.size(text)[0]) // 2,
                           WINDOW_HEIGHT // position))
 
-    def draw_button(self, text, coords):
-        """Draw a button."""
+    def draw_button(self, text, coordinates):
+        """Draw button."""
         pygame.draw.rect(self.window, (130, 180, 255), (
-            coords[0], coords[1], BUTTON_WIDTH, BUTTON_HEIGHT))
+            coordinates[0], coordinates[1], BUTTON_WIDTH, BUTTON_HEIGHT))
         text_width, text_height = self.font40.size(text)
         self.window.blit(self.font40.render(text, True, (0, 0, 0)),
-                         (coords[0] + (BUTTON_WIDTH - text_width) // 2,
-                          coords[1] + (BUTTON_HEIGHT - text_height) // 2))
-
-    def draw_two_buttons(self, upper_button, lower_button):
-        """Draw two buttons on the same page and return their coordinates."""
-        upper_button_coords = ((WINDOW_WIDTH - BUTTON_WIDTH) // 2,
-                               WINDOW_HEIGHT // 10 * 7)
-        self.draw_button(upper_button, upper_button_coords)
-        lower_button_coords = ((WINDOW_WIDTH - BUTTON_WIDTH) // 2,
-                               WINDOW_HEIGHT // 10 * 8)
-        self.draw_button(lower_button, lower_button_coords)
-        return [upper_button_coords, lower_button_coords]
+                         (coordinates[0] + (BUTTON_WIDTH - text_width) // 2,
+                          coordinates[1] + (BUTTON_HEIGHT - text_height) // 2))
 
     @staticmethod
-    def is_button_clicked(coords):
+    def is_button_clicked(coordinates):
         """Check whether a button gets clicked."""
-        return True if coords[0] <= pygame.mouse.get_pos()[0] < \
-                       coords[0] + BUTTON_WIDTH and coords[1] < \
+        return True if coordinates[0] <= pygame.mouse.get_pos()[0] < \
+                       coordinates[0] + BUTTON_WIDTH and coordinates[1] < \
                        pygame.mouse.get_pos()[1] < \
-                       coords[1] + BUTTON_HEIGHT else False
-
-    @staticmethod
-    def get_result():
-        """Get game result from the Game class."""
-        if Game.result[1]['checkmate']:
-            return [(Game.result[0] + ' wins').title(),
-                    ('by ' + 'checkmate').title()]
-        elif Game.result[1]['stalemate']:
-            return ['Draw', ('by ' + 'stalemate').title()]
+                       coordinates[1] + BUTTON_HEIGHT else False
 
     def main_menu(self):
-        """Build the main menu."""
-        self.window.fill((220, 220, 220))  # fills background color
-        self.draw_title_surface()
+        """Customize a menu page for the main menu."""
         self.draw_texture()
         self.draw_text('108', 'CHESS', 4)
-        button_coords = self.draw_two_buttons('Start', 'Exit')
-        while True:
-            self.clock.tick(FPS)
-            pygame.display.flip()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.ongoing = False
-                    return
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    # check whether the Start button is clicked
-                    if self.is_button_clicked(button_coords[0]):
-                        self.in_main_menu = False
-                        return
-                    # check whether the Exit button is clicked
-                    elif self.is_button_clicked(button_coords[1]):
-                        self.ongoing = False
-                        return
+        # return button names and the corresponding actions
+        return ('Start', 'Exit'), (('in_main_menu', False), ('ongoing', False))
 
     def result_page(self):
-        """Build the result page."""
+        """Customize a menu page for the result page."""
+        result, cause = '', ''
+        if Game.result[1]['checkmate']:
+            result, cause = Game.result[0] + ' wins', 'by ' + 'checkmate'
+        elif Game.result[1]['stalemate']:
+            result, cause = 'draw', 'by ' + 'stalemate'
+        self.draw_text('108', result.title(), 5)
+        self.draw_text('40', cause.title(), 3)
+        # return button names and the corresponding actions
+        return ('New Game', 'Main Menu'), (None, ('in_main_menu', True))
+
+    def menu_page(self, page_name):
+        """Build menu pages."""
         self.window.fill((220, 220, 220))  # fills background color
         self.draw_title_surface()
-        self.draw_text('108', self.get_result()[0], 5)
-        self.draw_text('40', self.get_result()[1], 3)
-        button_coords = self.draw_two_buttons('New Game', 'Main Menu')
+        button_names, actions = getattr(Menu, page_name)(self)
+        button_coordinates = (
+            ((WINDOW_WIDTH - BUTTON_WIDTH) // 2, WINDOW_HEIGHT // 10 * 7),
+            ((WINDOW_WIDTH - BUTTON_WIDTH) // 2, WINDOW_HEIGHT // 10 * 8))
+        for index, button_coordinate in enumerate(button_coordinates):
+            self.draw_button(button_names[index], button_coordinate)
         while True:
             self.clock.tick(FPS)
             pygame.display.flip()
@@ -118,24 +97,24 @@ class Menu(Game):
                     self.ongoing = False
                     return
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    # check whether the New Game button is clicked
-                    if self.is_button_clicked(button_coords[0]):
-                        return
-                    # check whether the Main Menu button is clicked
-                    elif self.is_button_clicked(button_coords[1]):
-                        self.in_main_menu = True
-                        return
+                    for index, action in enumerate(actions):
+                        # check which button is clicked and take the
+                        # corresponding action to it
+                        if self.is_button_clicked(button_coordinates[index]):
+                            if action:
+                                self.__dict__[action[0]] = action[1]
+                            return
 
     def menu(self):
-        """Meld main menu and result page with the chess game."""
+        """Meld menu pages with the chess game."""
         while self.ongoing:
             if self.in_main_menu:
-                self.main_menu()
+                self.menu_page('main_menu')
             else:
                 self.ongoing = Game().play()
                 if not self.ongoing:
                     return
-                self.result_page()
+                self.menu_page('result_page')
 
 
 if __name__ == '__main__':
